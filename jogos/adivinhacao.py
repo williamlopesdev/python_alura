951af7c877e1cab3aab4fe690514e86fd4ef2794
williamlopesdev <wilcypherr@gmail.com>
2022-09-09 15:01:00 -0300

    numero de tentativas por interpolação de strings

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -8,7 +8,7 @@ rodada = 1
 
 
 while(rodada <= total_de_tentativas):
-    print("Tentativa: ", rodada, "de", total_de_tentativas)
+    print("Tentativa:  {} de {}".format(rodada, total_de_tentativas))
     chute_str = input("Digite o seu numero: ")
     print("Você digitou ", chute_str)
     chute = int(chute_str)
