81015bb3daa9fdc55df089fd41e01608a4a71ac9
williamlopesdev <wilcypherr@gmail.com>
2022-09-09 14:27:15 -0300

    adicionando total de tentativas e refazendo as variaveis

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -3,16 +3,28 @@ print("Bem vindo no jogo de Adivinhação!")
 print("***************************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute_str = input("Digite o seu numero: ")
 
-print("Você digitou ", chute_str)
+while(rodada <= total_de_tentativas):
+    print("Tentativa: ", rodada, "de", total_de_tentativas)
+    chute_str = input("Digite o seu numero: ")
+    print("Você digitou ", chute_str)
+    chute = int(chute_str)
 
-chute = int(chute_str)
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-if (numero_secreto == chute):
-    print("Você acertou")
-else:
-    print("Você errou")
+    if (acertou):
+        print("Você acertou")
+    else:
+        if (maior):
+            print("Você errou! O seu chute foi maior que o número secreto")
+        elif (menor ):
+            print("Você errou! O seu chute foi menor que o número secreto")
+
+    rodada = rodada + 1
 
 print("Fim do Jogo")
