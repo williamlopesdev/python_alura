5205a9c435cf3af927d5e776ec7c9563078d61b2
williamlopesdev <wilcypherr@gmail.com>
2022-09-11 10:29:35 -0300

    numero secreto sendo gerado de forma aleratória com a função randrange()

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,10 +1,15 @@
+import random
+
+
 print("***************************************")
 print("Bem vindo no jogo de Adivinhação!")
 print("***************************************")
 
-numero_secreto = 42
+
+numero_secreto = random.randrange(1, 101)
 total_de_tentativas = 3
 
+print(numero_secreto)
 
 for rodada in range(1, total_de_tentativas +1):
     print("Tentativa:  {} de {}".format(rodada, total_de_tentativas))
