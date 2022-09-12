2cfda7c1288493b32d87706bdf46529de08e3a2e
williamlopesdev <wilcypherr@gmail.com>
2022-09-12 16:48:54 -0300

    _ com o mesmo tamanho da palanha secreta

--- jogos/forca.py
+++ jogos/forca.py
@@ -4,27 +4,37 @@ def jogar_forca():
     print("Bem vindo no jogo de Forca!")
     print("***************************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    palavra_secreta = "uva".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
 
     while(not enforcou and not acertou):
 
         chute = input("Qual letra?")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if (chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if (chute.upper() == letra.upper()):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
 
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
 
+    if(acertou):
+        print("Você ganhou!")
+    else:
+        print("Você perdeu.")
 
     print("Fim do Jogo")
 
