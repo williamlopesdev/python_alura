fc86eac0b04d7157c295c21e365097c609435539
williamlopesdev <wilcypherr@gmail.com>
2022-09-12 11:09:12 -0300

    validação para encontrar a letra e sua posição na palavra

--- jogos/forca.py
+++ jogos/forca.py
@@ -4,6 +4,22 @@ def jogar_forca():
     print("Bem vindo no jogo de Forca!")
     print("***************************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra?")
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute == letra):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+
+        print("jogando...")
 
 
     print("Fim do Jogo")
